9322fe81bc25da88ea404566c9915601ef726963
Tim Heuer <tim@timheuer.com>
2023-02-14 10:18:15 -0800

    Add HelperFunctions to provide a custom commit prompt.

---
+++ HelperFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aicommits;
+internal class HelperFunctions
+{
+    public static string GetPrompt()
+    {
+        return @"[bold white]Commit message: {0}\n[/]";
+    }
+}
